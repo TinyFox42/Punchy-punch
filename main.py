4e918896af632a3f69a92e870cceca2298196e9e
Eli Thorpe <elithorpe42@gmail.com>
2019-05-18 15:42:10 -0400

    Start the ability system
    More little development steps as I do other stuff to get out of a panic attack (it isn't working... :frowning: )

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class fighter(object):
         self.mhp=mhp
         self.ac=ac
         self.hp=mhp
+        self.skills=[]
     def __str__(self):
         #When you try to print it normally, 
         return "%10s (%3ld%s)"%(self.name, ((100*self.hp)/self.mhp),'%')
@@ -28,4 +29,23 @@ class fighter(object):
                 return "%s was hit for %d damage! %s has been defeated!"%(self.name, d, self.name)
         else:
             return "%s blocked the attack!"%(self.name)
+    def learn(self, abil):
+        self.skills.append(abil)
+    def forget(self, abil):
+        #Removes an ability
+        #Note- you give this function the specific ability OBJECT, not just an object of the same ability type
+        self.skills.remove(abil)
 
+class abil(object):
+    #Does Python have abstract classes? This is an abstract class, pretty much
+    def __init__(self, lvl=0):
+        self.name="Confetti"
+        #Stuff that won't matter until much later in development:
+        self.lvl=lvl
+        self.sp=0 #mana/stamina cost
+        self.tm=0 #time cost
+    def use(self, tar):
+        #tar is the fighter that is the target of the ability
+        return "%s is showered in confetti! It has litterally no effect!"%(tar.name)
+    def __str__(self):
+        return "%10s (lvl.%2d)"%(self.name, self.lvl)

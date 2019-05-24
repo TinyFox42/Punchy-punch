44ba3e84af223caaf0624fcfabe6608662f7fdbb
Eli Thorpe <elithorpe42@gmail.com>
2019-05-24 11:00:40 -0400

    Implement turn system
    Now the abilities are consistent with the main file, at least in the context of the turn system.

--- abilities.py
+++ abilities.py
@@ -22,6 +22,15 @@ class punch(abil):
         self.sp=0
         self.tm=5
     def use(self, usr, tar):
-        #usr.wait(5) #user needs to wait 5 time units to use the ability
-        tar.defend(5) #the target is hit for 5 damage
-        
+        usr.wait(self.tm, self.activate, [usr, tar]) #user needs to wait 5 time units to use the ability
+        return "%s starts building up for a punch!"%(usr.name)
+        #tar.defend(5) #the target is hit for 5 damage
+    def activate(self, data):
+        #data[0] is the user object
+        #data[1] is the targtet object
+        usr=data[0]
+        tar=data[1]
+        n="%s punches %s!"%(usr.name, tar.name)
+        n+="\n"
+        n+=tar.defend(5)
+        return n

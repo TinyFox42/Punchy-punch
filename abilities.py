9addad55b375c758e3eb685456b710f9b01dd0aa
Eli Thorpe <elithorpe42@gmail.com>
2019-05-24 10:41:40 -0400

    Add in the punch ability
    A test ability.
    I currently have nothing productive to do, so I am just working on this project.
    I am using the web editor, so I am unable to actually test this code well, or even decently check syntax, so I'm using this different branch.
    (Also, I have to make a commit for every file change, instead of whenever I want to save my edits)

--- abilities.py
+++ abilities.py
@@ -8,9 +8,20 @@ class abil(object):
         self.lvl=lvl
         self.sp=0 #mana/stamina cost
         self.tm=0 #time cost
-    def use(self, tar):
+    def use(self, usr, tar):
         #tar is the fighter that is the target of the ability
-        return "%s is showered in confetti! It has litterally no effect!"%(tar.name)
+        #usr is the fighter that activated the ability
+        return "%s is showered in confetti by %s! It has litterally no effect!"%(tar.name, usr.name)
     def __str__(self):
         return "%10s (lvl.%2d)"%(self.name, self.lvl)
+        
+class punch(abil):
+    def __init__(self, lvl=0):
+        self.name="Punch"
+        self.lvl=lvl
+        self.sp=0
+        self.tm=5
+    def use(self, usr, tar):
+        #usr.wait(5) #user needs to wait 5 time units to use the ability
+        tar.defend(5) #the target is hit for 5 damage
         
